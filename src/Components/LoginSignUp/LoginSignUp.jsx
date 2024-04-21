d01324e4889b87dce2111b6cd7d160f957e5c6d8
nazlifs <nazlifadlan@gmail.com>
2024-04-21 16:29:28 +0700

    Lost Password -> Forgot Password

--- src/Components/LoginSignUp/LoginSignUp.jsx
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -30,7 +30,7 @@ const [action,setAction] = useState("Login")
           <input type='password' placeholder='Password'/>
         </div>
       </div>
-      {action === "Sign Up"? <div></div>:<div className='forgot-password'>Lost Password <span>Click Here!</span></div>}
+      {action === "Sign Up"? <div></div>:<div className='forgot-password'>Forgot Password <span>Click Here!</span></div>}
       
       <div className='submit-container'>
         <div className={action === "Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
